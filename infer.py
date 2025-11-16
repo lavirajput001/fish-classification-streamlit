d310c3c3242b302dfd53d6d1a29f9b3568ff5ad6
lavi rajput <121419564+lavirajput001@users.noreply.github.com>
2025-11-16 20:40:30 +0530

    Update infer.py

--- infer.py
+++ infer.py
@@ -2,11 +2,16 @@ import numpy as np
 import cv2
 
 def final_predict(model, image, label_encoder):
-    img = cv2.resize(image, (96, 96))   # CNN input size
-    img = img.astype('float32') / 255.0
-    img = np.expand_dims(img, axis=0)   # (1, 96, 96, 3)
+    # If image is None -> error
+    if image is None:
+        raise ValueError("Image not loaded! Received None.")
 
-    pred = model.predict(img)
-    index = np.argmax(pred)
+    # Resize for CNN
+    img = cv2.resize(image, (96, 96))
+    img = img.astype("float32") / 255.0
+    img = np.expand_dims(img, axis=0)   # (1,96,96,3)
+
+    preds = model.predict(img)
+    index = np.argmax(preds)
     label = label_encoder.inverse_transform([index])[0]
     return label
